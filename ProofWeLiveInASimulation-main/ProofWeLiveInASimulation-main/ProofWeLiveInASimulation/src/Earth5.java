4674ef8f584025efe65de7dae80ba4b945aacd29
George Cook <thegekko07@gmail.com>
2023-11-07 12:41:06 +0000

    Added JavaDoc to Person Class
     and added comments in Earth5 Class, to help with readablity.

--- ProofWeLiveInASimulation-main/ProofWeLiveInASimulation-main/ProofWeLiveInASimulation/src/Earth5.java
+++ ProofWeLiveInASimulation-main/ProofWeLiveInASimulation-main/ProofWeLiveInASimulation/src/Earth5.java
@@ -1,8 +1,13 @@
 //import java.math.BigInteger;
-
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Main class that represents the earth and loops through the whole population.
+ * Each loop represents a year on earth.
+ * @version 5
+ * @author George Cook
+ */
 public class Earth5 {
 
     public static void main(String[] args) {
@@ -17,6 +22,9 @@ public class Earth5 {
         int startingPopulationSize;
 //        BigInteger worldMoney =  BigInteger.valueOf(0);
 
+        /*
+        Setting up the initial population with one male and one female.
+         */
         for (int s = 0; s <= 1; s++) {
             Person person;
             if (s == 0) {
@@ -29,6 +37,9 @@ public class Earth5 {
 
         }
 
+        /*
+        Shows the user the starting state of the population.
+         */
         System.out.println("Starting State:");
         for (Person value : population) {
             System.out.println(value.getName() + " age: " + value.getAge() + " sex: " + value.getSex() + " year born: " + value.getYearBorn());
@@ -36,8 +47,15 @@ public class Earth5 {
 
         startingPopulationSize = population.size();
 
+        /*
+        Start of the do while loop that continues until the population reaches 0.
+         */
         do {
 
+            /*
+            Start of the for loop that goes through the whole array of population.
+            Representing a year for every full loop through the population array.
+             */
             for (Person person : population) {
 
                 Person person2 = population.get(rand.nextInt(startingPopulationSize));
@@ -68,7 +86,7 @@ public class Earth5 {
                 }
                 person.haveABirthday();
 
-                if (person.sex == 'F') {
+                if (person.getSex() == 'F') {
                     person.giveBirth();
                 }
 /*
@@ -78,9 +96,16 @@ public class Earth5 {
 */
             }
 
+            /*
+            Removes all the dead people from the population.
+            Adds all the people born to the population.
+             */
             population.removeAll(itemsToRemove);
             population.addAll(itemsToAdd);
 
+            /*
+            If statement that prints the current state of the earth every 50 years.
+             */
             if (year % 50 == 0) {
 //                BigInteger mph = worldMoney.divide(BigInteger.valueOf(population.size()));
                 System.out.printf("\nYear: %d\nPeople Born: %d\nPeople Dead: %d\nTotal Population: %d%n", year, peopleBorn, peopleDead, population.size());
@@ -90,6 +115,10 @@ public class Earth5 {
 */
             }
 
+            /*
+            Increases the current year by one.
+            And then resets all the necessary variable to their needed values for the next year.
+             */
             year++;
             peopleBorn = 0;
             peopleDead = 0;
--- ProofWeLiveInASimulation-main/ProofWeLiveInASimulation-main/ProofWeLiveInASimulation/src/Person.java
+++ ProofWeLiveInASimulation-main/ProofWeLiveInASimulation-main/ProofWeLiveInASimulation/src/Person.java
@@ -1,21 +1,26 @@
 import java.util.Random;
 
+/**
+ * Person Class that represents a person in on the earth.
+ * Holds all the methods that a person will need throughout their lifetime.
+ * @author George Cook
+ */
 public class Person {
 
-    String name;
-    char sex;
-    int age;
-    int yearBorn;
-    boolean pregnant;
+    private String name;
+    private char sex;
+    private int age;
+    private int yearBorn;
+    private boolean pregnant;
+    private int deathYear = -1;
+    private int money;
+    private int numberOfChildren;
+    private int lastChild;
 
-    int deathYear = -1;
-
-    int money;
-
-    int numberOfChildren;
-
-    int lastChild;
-
+    /**
+     * Constructor for Person class that sudo randomly assigns Sex.
+     * @param yearBorn
+     */
     Person(int yearBorn) {
         chooseSex();
         chooseName();
@@ -28,6 +33,12 @@ public class Person {
         this.lastChild = 0;
         this.numberOfChildren = 0;
     }
+
+    /**
+     * Constructor for Person class that allows you to set the Sex.
+     * @param yearBorn
+     * @param sex
+     */
     Person(int yearBorn, char sex) {
         this.sex = sex;
         chooseName();
@@ -41,98 +52,181 @@ public class Person {
         this.numberOfChildren = 0;
     }
 
+    /**
+     * Getter that returns the Name of the Person.
+     * @return name variable that represents the person's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter that allows the changing of the name of the person.
+     * @param name The new String value that will be assigned to the name variable.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Setter that allows the changing of the sex of the person.
+     * @param sex The new Char value that will be assigned to the sex variable.
+     */
     public void setSex(char sex) {
         this.sex = sex;
     }
 
+    /**
+     * Setter that allows the changing of the age of the person.
+     * @param age The new int value that will be assigned to the age variable.
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * Getter that returns the Year that the person has been born.
+     * @return yearBorn variable that represents the year the person was born.
+     */
     public int getYearBorn() {
         return yearBorn;
     }
 
+    /**
+     * Setter that allows the changing of the year the person was born.
+     * @param yearBorn The new int value that will be assigned to the yearBorn variable.
+     */
     public void setYearBorn(int yearBorn) {
         this.yearBorn = yearBorn;
     }
 
+    /**
+     * Getter that return the boolean value of if the person is pregnant or not.
+     * @return pregnant variable that represents in the person is pregnant.
+     */
     public boolean isPregnant() {
         return pregnant;
     }
 
+    /**
+     * Setter that allows the changing of if the person is pregnant or not.
+     * @param pregnant The new boolean value that will be assigned to the pregnant variable.
+     */
     public void setPregnant(boolean pregnant) {
         this.pregnant = pregnant;
     }
 
+    /**
+     * Getter that returns the Year that the person has is going to die.
+     * @return deathYear variable that represents the year the person will die.
+     */
     public int getDeathYear() {
         return deathYear;
     }
 
+    /**
+     * Setter that allows the changing of the year the person will die.
+     * @param deathYear The new int value that will be assigned to the deathYear variable.
+     */
     public void setDeathYear(int deathYear) {
         this.deathYear = deathYear;
     }
 
+    /**
+     * Setter that allows the changing of the amount of money the person has.
+     * @param money The new int value that will be assigned to the money variable.
+     */
     public void setMoney(int money) {
         this.money = money;
     }
 
+    /**
+     * Getter that returns the number of children the person has.
+     * @return numberOfChildren variable that represents the number of children that the person has.
+     */
     public int getNumberOfChildren() {
         return numberOfChildren;
     }
 
+    /**
+     * Setter that allows the changing of the number of children a person has had.
+     * @param numberOfChildren The new int value that will be assigned to the numberOfChildren variable.
+     */
     public void setNumberOfChildren(int numberOfChildren) {
         this.numberOfChildren = numberOfChildren;
     }
 
+    /**
+     * Getter that returns the year that the person last had a child.
+     * @return lastChild variable that represents the year that the person last had a child.
+     */
     public int getLastChild() {
         return lastChild;
     }
 
+    /**
+     * Setter that allows the changing of the year that the person last had a child.
+     * @param lastChild The new int value that will be assigned to the lastChild variable.
+     */
     public void setLastChild(int lastChild) {
         this.lastChild = lastChild;
     }
 
-    public void haveABirthday() {
-        age += 1;
-    }
-
+    /**
+     * Getter that returns the age of the person.
+     * @return age variable that represents the age of the person.
+     */
     public int getAge() {
         return this.age;
     }
 
+    /**
+     * Getter that returns the sex of the person.
+     * @return sex variable that represents the sex of the person.
+     */
     public char getSex() {
         return this.sex;
     }
 
+    /**
+     * Getter that returns the amount of money that the person has.
+     * @return money variable that represents the amount of money that the person has.
+     */
     public int getMoney() {
         return this.money;
     }
 
+    /**
+     * Method that pays the person their yearly salary.
+     * @param salary The int value that represents the persons yearly salary that is added to their total money.
+     */
     public void getPaid(int salary) {
         this.money += salary;
     }
 
+    /**
+     * Method that takes money off of the persons total money.
+     * Represents them paying their total out goings for the year.
+     */
     public void payBills() {
         this.money -= 26062 / 2;
     }
 
+    /**
+     * Method that increases the persons age by one year.
+     */
+    public void haveABirthday() {
+        age += 1;
+    }
 
+    /**
+     * Method that chooses the name of the person based on their sex.
+     */
     private void chooseName() {
         int choice;
 
         Random rand = new Random();
         choice = rand.nextInt(3);
-        //System.out.println(choice);
-
         if (this.sex == 'M') {
             switch (choice) {
                 case 0:
@@ -162,12 +256,14 @@ public class Person {
         }
     }
 
+    /**
+     * Method that sudo randomly sets the person's sex.
+     */
     private void chooseSex() {
         int choice;
 
         Random rand = new Random();
         choice = rand.nextInt(2);
-
         switch (choice) {
             case 0:
                 this.sex = 'M';
@@ -178,6 +274,12 @@ public class Person {
         }
     }
 
+    /**
+     * Method that calculates if the person is going to die based on current age.
+     * Based on real world data.
+     * @param currentYear The int value that represents the current year.
+     * @return boolean value that says whether a person is going to die this year.
+     */
     public boolean chanceOfDeath(int currentYear) {
         double death = 0, deathPercentage, percentage;
 
@@ -234,7 +336,6 @@ public class Person {
         Random rand = new Random();
         percentage = rand.nextDouble();
         deathPercentage = (death / 100);
-
         if (percentage <= deathPercentage || deathYear == currentYear) {
             return true;
         } else {
@@ -243,6 +344,17 @@ public class Person {
 
     }
 
+    /**
+     * Method that calculates if the person is going to have a child.
+     * Based on real world data
+     * @param partnerAge The int value that represents the age of the current partner
+     * @param partnerSex The char value that represents the sex of the current partner
+     * @param isPregnant The boolean value that represents the pregnancy state of the current partner
+     * @param partnerNumberOfChildren The int value that represents the number of children of the current partner
+     * @param year The int value that represents the current year.
+     * @param partnerLastChild The int value that represents the year of the last child of the current partner
+     * @return boolean value that says whether a person is going to have a child this year.
+     */
     public boolean haveAChild(int partnerAge, char partnerSex, boolean isPregnant, int partnerNumberOfChildren, int year, int partnerLastChild) {
         int chanceOfConservation;
 
@@ -263,6 +375,11 @@ public class Person {
         return false;
     }
 
+    /**
+     * Method that changes the state of pregnant to True.
+     * And then sets the year that the person last had a child to the current year.
+     * @param year The int value that represents the current year.
+     */
     public void becomePregnant(int year) {
         //lastChild = year;
         if (this.sex == 'F') {
@@ -271,11 +388,20 @@ public class Person {
         }
     }
 
+    /**
+     * Method that changes the state of pregnant to false.
+     * And then increases the number of children that the person has had by one.
+     */
     public void giveBirth() {
         this.pregnant = false;
         numberOfChildren = numberOfChildren + 1;
     }
 
+    /**
+     * Method that calculates whether a person is going to die of terminal cancer next year.
+     * And sets the deathYear to one plus the currentYear.
+     * @param currentYear The int value that represents the current year.
+     */
     public void chanceOfFatalCancer(int currentYear) {
         double death, deathPercentage, percentage;
 
@@ -289,7 +415,6 @@ public class Person {
 
         percentage = rand.nextDouble();
         deathPercentage = (death / 100);
-
         if ((percentage <= deathPercentage) && (this.age >= 18)) {
             this.deathYear = currentYear + 1;
         } else if ((percentage <= (deathPercentage / 2)) && (this.age < 18)) {
